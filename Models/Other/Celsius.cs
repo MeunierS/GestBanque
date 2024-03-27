9665c9ef35db8ec259459c7a0606e05df3aa93ed
Meunier Sébastien <meuniersebastien1987@gmail.com>
2024-03-27 09:05:18 +0100

    change in organization of files

--- Models/Other/Celsius.cs
+++ Models/Other/Celsius.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models
+namespace Models.Other
 {
     public class Celsius
     {
         public double Temperature { get; set; }
 
-        public static implicit operator Fahrenheit (Celsius celsius)
+        public static implicit operator Fahrenheit(Celsius celsius)
         {
-            Fahrenheit fahrenheit = new Fahrenheit ();
+            Fahrenheit fahrenheit = new Fahrenheit();
             fahrenheit.Temperature = celsius.Temperature * 1.8 + 32;
             return fahrenheit;
         }
